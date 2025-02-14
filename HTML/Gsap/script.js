ef7f160b2abee80422447783aba78f8dc65ab146
Nehamalwal <nehaverma5017@gmail.com>
2025-02-14 11:42:11 +0530

    add files

---
+++ HTML/Gsap/script.js
@@ -0,0 +1,17 @@
+gsap.to(".box",{
+    x:600,
+    duration:1.2,
+    stagger:1,
+    delay:1,
+    borderRadius:("75%")
+
+})
+gsap.to(".box1",{
+    x:-600,
+    duration:1.2,
+    stagger:1,
+    delay:1,
+    borderRadius:("75%"),
+    
+
+})
